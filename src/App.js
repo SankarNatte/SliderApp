f0bd491861db08522fb023b34df979b0d0a94efa
Njssankar <sankarnatte@gmail.com>
2024-07-24 15:21:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+function App(){
+  const msg=["Message-one","Message-Two","Message-Three"];
+ const[pg,setpg]=useState(1)
+  function clickprevious(){
+setpg(pg-1);
+  }
+  function clicknext(){
+setpg(pg+1);
+  }
+  return(
+    <div className='page'>
+      <div className='number'>
+<div id="p" className={pg===1?" active":" "}>1</div>
+<div id="p" className={pg===2?" active":" "}>2</div>
+<div id="p" className={pg===3?" active":" "}>3</div>
+      </div>
+      <div className='message'>
+<div>{msg[pg-1]}</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+  
+      <div className='buttons'>
+<button onClick={clickprevious}>Previous</button>
+<button onClick={clicknext}>Next</button>
+      </div>
     </div>
+    
   );
 }
-
 export default App;
